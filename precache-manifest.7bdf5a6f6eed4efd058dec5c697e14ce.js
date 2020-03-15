ea64130844bbe97d483cff7f4e35976e3d2a3bc6
naderalfakesh <naderalfakesh@gmail.com>
2020-03-15 23:53:48 +0300

    Updates

---
+++ precache-manifest.7bdf5a6f6eed4efd058dec5c697e14ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54a214852e18655b221c278b4e192bc",
+    "url": "/servis-takip-sistemi/index.html"
+  },
+  {
+    "revision": "3fb1f967f3eae47002d9",
+    "url": "/servis-takip-sistemi/static/css/main.28bac1de.chunk.css"
+  },
+  {
+    "revision": "3f42d0ac6d5f85f7fc2f",
+    "url": "/servis-takip-sistemi/static/js/2.3513aa76.chunk.js"
+  },
+  {
+    "revision": "739ebc9ac96102ee664c12836372f36d",
+    "url": "/servis-takip-sistemi/static/js/2.3513aa76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb1f967f3eae47002d9",
+    "url": "/servis-takip-sistemi/static/js/main.ced273e8.chunk.js"
+  },
+  {
+    "revision": "cc115c3927ecc02beee7",
+    "url": "/servis-takip-sistemi/static/js/runtime-main.82f6d52d.js"
+  }
+]);
